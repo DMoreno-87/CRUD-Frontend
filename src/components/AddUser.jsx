9e5be60890faee55775714331a5034782789da0c
cranberry <jocsan.rodriguez89@stu.bmcc.cuny.edu>
2025-07-07 15:45:06 -0400

    Made changes to AddUser

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React,{useState} from 'react';
+
 
 const AddUser = () => {
+
+    const [form, setForm] = useState(
+        {
+            userName: "",
+            passwordHash: "",
+        }
+    )
+
+    console.log("form-->:", form);
+    console.log("hello world");
+
+    const handleSubmit = (e) => {
+       e.preventDefault();
+     
+       
+
+       setForm()
+    }
     return (
         <main>
             <h1>Create User</h1>
@@ -8,19 +27,24 @@ const AddUser = () => {
                 <label>Username:</label>
                 <input
                     type='text'
-                    name='username'
+                    name='userName'
+                    value={form.userName}
+                    onChange={(e) => setForm({...form, [e.target.name]: e.target.value})}
                     required
                 />
                 <label>Password</label>
                 <input
                     type='pasword'
                     name='passwordHASH'
+                    value={form.passwordHash}
+                    onChange={(e) => setForm({...form, [e.target.name]: e.target.value})}
                     required
                 />
                 <button>Submit</button>
             </form>
         </main>
     )
-}
+
+};
 
 export default AddUser
